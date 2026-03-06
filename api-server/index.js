65b898499e3303b79692cc3ae42e1eb76a629ea7
Rashmi Ranjan Parija <126317142+rashmiofficial45@users.noreply.github.com>
2026-03-07 03:32:16 +0530

    feat: Implement real-time build log streaming using Socket.IO and Redis pub/sub.

--- api-server/index.js
+++ api-server/index.js
@@ -1,10 +1,11 @@
 const express = require("express")
 const { ECSClient, RunTaskCommand } = require("@aws-sdk/client-ecs")
 const dotenv = require("dotenv")
+const Valkey = require("ioredis")
 const { generateSlug } = require("random-word-slugs")
 dotenv.config()
 const app = express()
-
+const { Server } = require("socket.io")
 app.use(express.json())
 
 const ecsClient = new ECSClient({
@@ -15,12 +16,27 @@ const ecsClient = new ECSClient({
   // }
 })
 
+const io = new Server({cors: { origin: "*" }})
+
+const subscriber = new Valkey("REMOVED_SECRET")
+
 const config = {
   CLUSTER_NAME: process.env.CLUSTER_NAME,
   TASK_DEFINITION: process.env.TASK_DEFINITION,
 }
 
-app.post("/project", async(req, res) => {
+io.on("connection", (socket) => {
+  socket.on("subscribe", (channel)=>{
+    socket.join(channel)
+    socket.emit("message",`Joined to ${channel}`)
+  } )
+})
+
+io.listen(9001, ()=>{
+  console.log("Socket server started on port 9001")
+})
+
+app.post("/project", async (req, res) => {
   const { gitUrl } = req.body
   console.log(gitUrl)
   const projectSlug = generateSlug()
@@ -57,9 +73,20 @@ app.post("/project", async(req, res) => {
       taskRoleArn: "arn:aws:iam::819720040633:role/ecs-build-worker-s3-upload",
     },
   })
+  console.log("Dispatching ECS task with PROJECT_ID:", projectSlug)
   await ecsClient.send(command)
   res.json({ status: "queued", projectSlug, deployUrl: `http://${projectSlug}.localhost:8000` })
 })
+
+async function initRedisSubscribe() {
+  console.log('Subscribed to logs....')
+  subscriber.psubscribe('logs:*')
+  subscriber.on('pmessage', (pattern, channel, message) => {
+    io.to(channel).emit('message', message)
+  })
+}
+
+initRedisSubscribe()
 
 app.listen(3000, () => {
   console.log("Server started on port 3000")
